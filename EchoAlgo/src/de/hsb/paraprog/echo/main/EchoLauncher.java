ba0b40ad2d6bdd3c83bab4335dc7db16a355dce6
Thorsten Kolling <thorsten.kolling@googlemail.com>
2016-08-20 12:21:53 +0200

    added and configured logback+slf4j logger

--- EchoAlgo/src/de/hsb/paraprog/echo/main/EchoLauncher.java
+++ EchoAlgo/src/de/hsb/paraprog/echo/main/EchoLauncher.java
@@ -1,10 +1,15 @@
 package de.hsb.paraprog.echo.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EchoLauncher {
+	
+	private static Logger logger = LoggerFactory.getLogger(EchoLauncher.class);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		logger.debug("logger test!");
 	}
 
 }
