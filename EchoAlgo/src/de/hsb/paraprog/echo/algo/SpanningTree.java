f2d8d818db7b0765d87402b08d38cf6ee6c2f0e2
Thorsten Kolling <thorsten.kolling@googlemail.com>
2016-08-26 21:35:03 +0200

    added spanning tree class and methods

---
+++ EchoAlgo/src/de/hsb/paraprog/echo/algo/SpanningTree.java
@@ -0,0 +1,45 @@
+package de.hsb.paraprog.echo.algo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.hsb.paraprog.echo.node.EchoNode;
+import de.hsb.paraprog.echo.node.Node;
+
+public class SpanningTree {
+	
+	private static Logger logger = LoggerFactory.getLogger(EchoNode.class);
+	
+	class TreeNode {
+		public TreeNode(Node node) {
+			this.node = node;
+			descendants = new HashSet<TreeNode>();
+		}
+		private Node node;
+		private Set<TreeNode> descendants;
+		
+		public void addDescendant(TreeNode treeNode) {
+			descendants.add(treeNode);
+		}
+		public void printSection() {
+			logger.info(node.toString());
+			for (TreeNode desc : descendants) {
+				desc.printSection();
+			}
+		}
+	}
+	
+	public SpanningTree(Node node) {
+		m_Root = new TreeNode(node);
+	}
+	private TreeNode m_Root;
+	
+	public void printTree() {
+		logger.info("spanning tree: ");
+		m_Root.printSection();
+	}
+	
+}
