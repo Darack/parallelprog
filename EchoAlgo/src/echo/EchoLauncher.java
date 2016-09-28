8541a41e81e83157595179a381cad5becc2ea26c
Thorsten Kolling <thorsten.kolling@googlemail.com>
2016-09-28 14:03:52 +0200

    added cli for both .jars

--- EchoAlgo/src/echo/EchoLauncher.java
+++ EchoAlgo/src/echo/EchoLauncher.java
@@ -4,41 +4,113 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EchoLauncher {
 	
 	private static Logger logger = LoggerFactory.getLogger(EchoLauncher.class);
+	private static int TESTCASES = 4;
+	private static String PROG_NAME = "ECHO_ALGO";
 
 	public static void main(String[] args) {
 		logger.debug("application start!");
+		
+		// command line options
+		Options options = new Options();
+		String testOption = "test";
+		options.addOption(testOption, true, "test case to check, pick a value between [1-" + TESTCASES + "]");
+		
+		// automatically generate the help statement
+		HelpFormatter formatter = new HelpFormatter();
+		
+		// default values
+		int testCase = 1;
+		
+		// get command line parameters
+		try {
+			CommandLineParser parser = new DefaultParser();
+			CommandLine cmd = parser.parse(options, args);
+			
+			logger.debug("debug");
+			
+			if(cmd.hasOption(testOption)) {
+				testCase = Integer.parseInt(cmd.getOptionValue(testOption));
+				logger.debug("testcase: " + testCase);
+				if (testCase > TESTCASES) {
+					throw new ParseException("wrong test case number!");
+				}
+			} else {
+				logger.debug("else");
+				formatter.printHelp( PROG_NAME, options, true );
+				System.exit(1);
+			}
+		} catch (ParseException e1) {
+			logger.debug("exception");
+			formatter.printHelp( PROG_NAME, options, true );
+			System.exit(1);
+		}
+				
 		CountDownLatch start = new CountDownLatch(1);
 		CountDownLatch end = new CountDownLatch(7);
 		List<EchoNode> echo = new ArrayList<EchoNode>();
 		
-		echo.add(new EchoNode("0", true, start, end));
-		
-//		echo.add(new EchoNode("1", false, start, end));
-//		echo.add(new EchoNode("2", false, start, end));
-//		echo.add(new EchoNode("3", false, start, end));
-//		echo.add(new EchoNode("4", false, start, end));
-//		echo.add(new EchoNode("5", false, start, end));
-//		echo.add(new EchoNode("6", false, start, end));
-//		echo.add(new EchoNode("7", false, start, end));
-		
-//		echo.get(0).setupNeighbours(echo.get(0));
-		
-//		echo.get(0).setupNeighbours(echo.get(1));
-//		echo.get(0).setupNeighbours(echo.get(2));
-//		echo.get(0).setupNeighbours(echo.get(3));
-//		echo.get(0).setupNeighbours(echo.get(4));
-//		echo.get(1).setupNeighbours(echo.get(2));
-////		echo.get(1).setupNeighbours(echo.get(5));
-//		echo.get(2).setupNeighbours(echo.get(3));
-//		echo.get(2).setupNeighbours(echo.get(5));
-//		echo.get(5).setupNeighbours(echo.get(6));
-//		echo.get(7).setupNeighbours(echo.get(0));
+		switch(testCase) {
+		case 1:
+			// create nodes
+			echo.add(new EchoNode("0", true, start, end));
+			
+			break;
+		case 2:
+			// create nodes
+			echo.add(new EchoNode("0", true, start, end));
+			
+			// create connections
+			echo.get(0).setupNeighbours(echo.get(0));
+			
+			break;
+		case 3:
+			// create nodes
+			echo.add(new EchoNode("0", true, start, end));
+			echo.add(new EchoNode("1", false, start, end));
+			echo.add(new EchoNode("2", false, start, end));
+			echo.add(new EchoNode("3", false, start, end));
+			echo.add(new EchoNode("4", false, start, end));
+			echo.add(new EchoNode("5", false, start, end));
+			
+			// create connections
+			echo.get(0).setupNeighbours(echo.get(1));
+			echo.get(1).setupNeighbours(echo.get(2));
+			echo.get(2).setupNeighbours(echo.get(3));
+			echo.get(2).setupNeighbours(echo.get(4));
+			echo.get(2).setupNeighbours(echo.get(5));
+			
+			break;
+		case 4:
+			// create nodes
+			echo.add(new EchoNode("0", true, start, end));
+			echo.add(new EchoNode("1", false, start, end));
+			echo.add(new EchoNode("2", false, start, end));
+			echo.add(new EchoNode("3", false, start, end));
+			echo.add(new EchoNode("4", false, start, end));
+			echo.add(new EchoNode("5", false, start, end));
+			
+			// create connections
+			echo.get(0).setupNeighbours(echo.get(1));
+			echo.get(1).setupNeighbours(echo.get(2));
+			echo.get(2).setupNeighbours(echo.get(3));
+			echo.get(2).setupNeighbours(echo.get(4));
+			echo.get(2).setupNeighbours(echo.get(5));
+			echo.get(0).setupNeighbours(echo.get(5));
+			
+			break;
+		}
 		
 		for (EchoNode echoNode : echo) {
 			echoNode.printNeighbours();
--- EchoAlgo/src/election/ElectionLauncher.java
+++ EchoAlgo/src/election/ElectionLauncher.java
@@ -4,66 +4,100 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import echo.EchoNode;
 
 public class ElectionLauncher {
 	
 	private static Logger logger = LoggerFactory.getLogger(ElectionLauncher.class);
+	private static int TESTCASES = 2;
+	private static String PROG_NAME = "ELECTION_ALGO";
 
 	public static void main(String[] args) {
 		logger.debug("application start!");
+		
+		// command line options
+		Options options = new Options();
+		String testOption = "test";
+		options.addOption(testOption, true, "test case to check, pick a value between [1-" + TESTCASES + "]");
+		
+		// automatically generate the help statement
+		HelpFormatter formatter = new HelpFormatter();
+		
+		// default values
+		int testCase = 1;
+		
+		// get command line parameters
+		try {
+			CommandLineParser parser = new DefaultParser();
+			CommandLine cmd = parser.parse(options, args);
+			
+			logger.debug("debug");
+			
+			if(cmd.hasOption(testOption)) {
+				testCase = Integer.parseInt(cmd.getOptionValue(testOption));
+				logger.debug("testcase: " + testCase);
+				if (testCase > TESTCASES) {
+					throw new ParseException("wrong test case number!");
+				}
+			} else {
+				logger.debug("else");
+				formatter.printHelp( PROG_NAME, options, true );
+				System.exit(1);
+			}
+		} catch (ParseException e1) {
+			logger.debug("exception");
+			formatter.printHelp( PROG_NAME, options, true );
+			System.exit(1);
+		}
+				
 		CountDownLatch start = new CountDownLatch(1);
 		List<ElectionNode> nodes = new ArrayList<ElectionNode>();
 		
-		nodes.add(new ElectionNode(0, start));
-		nodes.add(new ElectionNode(1, start));
-		nodes.add(new ElectionNode(2, start));
-		nodes.add(new ElectionNode(3, start));
-		nodes.add(new ElectionNode(4, start));
-		nodes.add(new ElectionNode(5, start));
-		nodes.add(new ElectionNode(6, start));
-		nodes.add(new ElectionNode(7, start));
-		nodes.add(new ElectionNode(8, start));
-		nodes.add(new ElectionNode(9, start));
-		nodes.add(new ElectionNode(10, start));
-		nodes.add(new ElectionNode(11, start));
-		
-
-		nodes.get(0).setupNeighbours(nodes.get(1));
-		nodes.get(0).setupNeighbours(nodes.get(2));
-		nodes.get(1).setupNeighbours(nodes.get(3));
-		nodes.get(3).setupNeighbours(nodes.get(4));
-		nodes.get(3).setupNeighbours(nodes.get(5));
-		nodes.get(3).setupNeighbours(nodes.get(6));
-		nodes.get(6).setupNeighbours(nodes.get(7));
-		nodes.get(7).setupNeighbours(nodes.get(8));
-		nodes.get(8).setupNeighbours(nodes.get(9));
-		nodes.get(9).setupNeighbours(nodes.get(10));
-		nodes.get(10).setupNeighbours(nodes.get(11));
-//		nodes.get(2).setupNeighbours(nodes.get(6));
-//		nodes.get(2).setupNeighbours(nodes.get(11));
-		
-//		echo.add(new ElectionNode("1", false, start, end));
-//		echo.add(new ElectionNode("2", false, start, end));
-//		echo.add(new ElectionNode("3", false, start, end));
-//		echo.add(new ElectionNode("4", false, start, end));
-//		echo.add(new ElectionNode("5", false, start, end));
-//		echo.add(new ElectionNode("6", false, start, end));
-//		echo.add(new ElectionNode("7", false, start, end));
-		
-//		echo.get(0).setupNeighbours(echo.get(0));
-		
-//		echo.get(0).setupNeighbours(echo.get(1));
-//		echo.get(0).setupNeighbours(echo.get(2));
-//		echo.get(0).setupNeighbours(echo.get(3));
-//		echo.get(0).setupNeighbours(echo.get(4));
-//		echo.get(1).setupNeighbours(echo.get(2));
-////		echo.get(1).setupNeighbours(echo.get(5));
-//		echo.get(2).setupNeighbours(echo.get(3));
-//		echo.get(2).setupNeighbours(echo.get(5));
-//		echo.get(5).setupNeighbours(echo.get(6));
-//		echo.get(7).setupNeighbours(echo.get(0));
+		switch(testCase) {
+		case 1:
+			// create nodes
+			nodes.add(new ElectionNode(0, start));
+			
+			break;
+		case 2:
+			// create nodes
+			nodes.add(new ElectionNode(0, start));
+			nodes.add(new ElectionNode(1, start));
+			nodes.add(new ElectionNode(2, start));
+			nodes.add(new ElectionNode(3, start));
+			nodes.add(new ElectionNode(4, start));
+			nodes.add(new ElectionNode(5, start));
+			nodes.add(new ElectionNode(6, start));
+			nodes.add(new ElectionNode(7, start));
+			nodes.add(new ElectionNode(8, start));
+			nodes.add(new ElectionNode(9, start));
+			nodes.add(new ElectionNode(10, start));
+			nodes.add(new ElectionNode(11, start));
+			
+			// create connections
+			nodes.get(0).setupNeighbours(nodes.get(1));
+			nodes.get(0).setupNeighbours(nodes.get(2));
+			nodes.get(1).setupNeighbours(nodes.get(3));
+			nodes.get(3).setupNeighbours(nodes.get(4));
+			nodes.get(3).setupNeighbours(nodes.get(5));
+			nodes.get(3).setupNeighbours(nodes.get(6));
+			nodes.get(6).setupNeighbours(nodes.get(7));
+			nodes.get(7).setupNeighbours(nodes.get(8));
+			nodes.get(8).setupNeighbours(nodes.get(9));
+			nodes.get(9).setupNeighbours(nodes.get(10));
+			nodes.get(10).setupNeighbours(nodes.get(11));
+			
+			break;
+		}
 		
 		for (ElectionNode ElectionNode : nodes) {
 			ElectionNode.printNeighbours();
